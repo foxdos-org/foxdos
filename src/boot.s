8cda624e501df9f9809786f3cc4e638e3b2ca974
mothcompute <mothcompute@protonmail.com>
2022-11-05 14:27:23 -0700

    some setup for filesystems

--- src/boot.s
+++ src/boot.s
@@ -36,5 +36,28 @@ retf
 
 ; TODO filesystem and reserve space for mbr partition table
 
+times 446 - ($-$$) db 0
+times bootpart*16 db 0
+
+; some programs do not accept values with bits 0-6 set
+status db 0
+
+; chs offset :/
+off_head db 0
+off_sector_cyl db 0
+off_cyl_low db 0
+
+; https://en.wikipedia.org/wiki/Partition_type
+type db 0
+
+; chs... end?
+end_head db 0
+end_sector_cyl db 0
+end_cyl_low db 0
+
+; lba :)
+lba_off dd 0
+lba_len dd 0
+	
 times 510 - ($-$$) db 0
 dw 0xAA55
